6038cd31d5fab162d60eb7c0f74a4b0fa450b79e
digiheadway <digiheadway@gmail.com>
2025-09-04 22:52:01 +0530

    imp gtm

--- pages/_app.js
+++ pages/_app.js
@@ -1,19 +1,9 @@
 import "../styles/globals.css";
 import Head from "next/head";
-import Script from "next/script";
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
-      {/* Google Tag Manager */}
-      <Script id="gtm-base" strategy="afterInteractive">
-        {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-P6ZMBHNC');`}
-      </Script>
-      {/* End Google Tag Manager */}
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       </Head>
